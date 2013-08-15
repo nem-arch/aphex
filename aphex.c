8aba61a7ae184f9a5f18d5fe54a61e173d72f71d
nem <nem@easy-monkey.co.uk>
2013-08-15 05:53:24 +0200

    modify main for new includes

--- aphex.c
+++ aphex.c
@@ -1,10 +1,13 @@
-#include <sys/mman.h>
-#include <stdio.h>
+
+#include "types.h"
 
 int main(int argc, char **argv)
 {
 	if (argc < 2 || (strcmp(argv[1], "-h") == 0)) {
 		printf("usage: %s <file>\n", argv[0]);
+		exit(EXIT_SUCCESS);
 	}
+	buf_open((unsigned char*)argv[1]);
+	buf_close();
 	return 0;
 }
