9a2f6cd3ebb0d170cdd45b6c145eeba665e7db2b
nem <nem@easy-monkey.co.uk>
2013-08-19 19:12:11 +0200

    change help if no argument given

--- aphex.c
+++ aphex.c
@@ -4,27 +4,10 @@
 int main(int argc, char **argv)
 {
 	if (argc < 2 || (strcmp(argv[1], "-h") == 0)) {
-		printf("aphex v0.3\n");
-		printf("\n");
 		printf("usage: %s [-r] <file>\n", argv[0]);
-		printf("-r: open file readonly\n");
+		printf("type 'man aphex' for controls and notes\n");
 		printf("\n");
 		printf("maximum filesize on system: %lu MB\n",LONG_MAX/1024/1024);
-		printf("\n");
-		printf("up, down, left, right: j, k, h, l\n");
-		printf("line start, line end: 0, $\n");
-		printf("file top, file bottom: gg, G\n");
-		printf("cursor +5 lines: 5j\n");
-		printf("cursor -10 lines: 10k\n");
-		printf("cursor +20 bytes: 20l\n");
-		printf("cursor -51 bytes: 51h\n");
-		printf("goto offset 0x10: 16g\n");
-		printf("quit: Q\n");
-		printf("redraw after resize: q (or any movement)\n");
-		printf("input mode: i\n");
-		printf("command mode: ESC\n");
-		printf("cycle edit mode: TAB\n");
-		printf("go back 1 byte in ascii edit: BACKSPACE\n");
 		exit(EXIT_SUCCESS);
 	}
 	if (strcmp(argv[1], "-r") == 0) {
